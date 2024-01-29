52f591ac1aa52ba94496302a10cafc5ba20f2fd9
XIYANG1018 <pkuyangxi@163.com>
2024-01-28 22:42:56 -0800

    updates

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ for (var i=0; i<numberOfDrumButtons; i++) {
         var buttonInnerHTML = this.innerHTML;
         
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
         
     });
 }
 
 
 document.addEventListener("keydown", function(event) {
-    makeSound(event.key)
+    makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 
@@ -48,6 +50,16 @@ function makeSound(key) {
             snare.play();
             break;
     }
+}
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
 
 
